51487122c5fc23bb7cf443a4550890fed429883d
camcimahir <99341536+camcimahir@users.noreply.github.com>
2025-10-10 16:16:28 +0000

    added automatic clicking logic and comments (step 3 complete)

--- src/main.ts
+++ src/main.ts
@@ -8,20 +8,32 @@ document.body.innerHTML = `
 
 let counter: number = 0;
 
+//the button
 const button = document.createElement("button");
 button.id = "increment";
 button.textContent = "🔥";
 
+//the counting number and text
 const counterElement = document.createElement("p");
 counterElement.id = "counter";
 counterElement.textContent = `You have ${counter} of fires`;
 
+//display the elements
 document.body.appendChild(button);
 document.body.appendChild(counterElement);
 
-button.addEventListener("click", () => {
-  // This looks like to a good place to add some logic!
+//updates the counter value and display
+const updateCounter = () => {
   counter++;
   counterElement.textContent = `You have ${counter} of fires`;
+};
+
+//every second it runs update counter
+setInterval(updateCounter, 1000);
+
+//every click runs the update counter
+button.addEventListener("click", () => {
+  // This looks like to a good place to add some logic!
+  updateCounter();
   console.log("I have these thingies:", button, counterElement, counter);
 });
