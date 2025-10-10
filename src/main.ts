a9500f157b9b4ed9d7a202ee64cc8f8e3bf6d779
camcimahir <99341536+camcimahir@users.noreply.github.com>
2025-10-10 15:28:00 +0000

    small changes to the html snippet

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,8 @@ import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 document.body.innerHTML = `
+  <h1>CMPM 121 Project</h1>
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <p>Counter: <span id="counter">0</span></p>
+  <button id="increment">Click Me!</button>
 `;
